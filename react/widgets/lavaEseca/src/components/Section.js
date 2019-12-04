b1943560a8fb200023fe0379193fb480a4de23de
Eduardo Soares <eduardo@visie.com.br>
2019-12-04 17:00:25 -0300

    lava e seca

--- react/widgets/lavaEseca/src/components/Section.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default props => {
-    return (
-        <section id={props.id} className={`${props.setClass} section-page`}>
-            {props.children}
-		</section>
-    )
-}
