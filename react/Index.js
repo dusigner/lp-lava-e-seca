9c8cb9cefd8992883159cde7f69cf904844fb0c9
Eduardo Soares <eduardo@visie.com.br>
2019-12-10 15:07:41 -0300

    addeding header and footer

--- react/Index.js
+++ react/Index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import SEO from 'brastemp.components';
+import { ExtensionPoint } from "render";
 import LavaESeca from "./lavaEseca/src/lavaESeca";
 import tagPlan from './lavaEseca/utils/tagPlan';
 
@@ -18,9 +19,9 @@ class lavaESeca extends Component {
             <React.Fragment>
                 <SEO title={"Lava e Seca | Encontre sua Máquina Lava e Seca | Brastemp"} description={"A lava e seca Brastemp é o modelo ideal para sua casa ou negócio. Confira as vantagens da melhor máquina lava e seca do mercado e compre online agora!"} />
                 <main id="main" className="main">
-                <ExtensionPoint id="header"  hideSupportMenu={true} headerType="lavaeseca"  />
+                <ExtensionPoint id="header"  hideSupportMenu={true} />
                 <LavaESeca />
-                <ExtensionPoint id="footer" footerType="lavaeseca"  />
+                <ExtensionPoint id="footer" />
                 </main>
             </React.Fragment>
         )
